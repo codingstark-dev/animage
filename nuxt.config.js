6f35d676ded6816636970a9bf18817242dd941d8
Himanshu Maurya <40685364+refoxdev@users.noreply.github.com>
2020-12-13 04:19:57 +0530

    bug fixed

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,9 @@ export default {
   },
   // Global CSS (https://go.nuxtjs.dev/config-css)
   css: [],
-
+ generate: {
+    fallback: true
+  },
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
     { src: "~/plugins/vue-stack-grid.js", mode: "client" },
